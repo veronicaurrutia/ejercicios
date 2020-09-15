5c950fba401b16b309f1975b370def0109c44cf7
vero <vero.urrutiaa@gmail.com>
2020-09-15 19:27:20 -0300

    Updates

---
+++ precache-manifest.d27a60ff044b3da29707c4aa4b884848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0e7f3e7d0b034e0a459aaf85feb109",
+    "url": "/ejercicios/index.html"
+  },
+  {
+    "revision": "039c33188f1d40b7fdbf",
+    "url": "/ejercicios/static/css/main.86e809dc.chunk.css"
+  },
+  {
+    "revision": "17385fd5d8e3ceb14afc",
+    "url": "/ejercicios/static/js/2.eb5f9227.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ejercicios/static/js/2.eb5f9227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039c33188f1d40b7fdbf",
+    "url": "/ejercicios/static/js/main.44cb5493.chunk.js"
+  },
+  {
+    "revision": "0c5850ed1f010f863707",
+    "url": "/ejercicios/static/js/runtime-main.6a2c0b4d.js"
+  }
+]);
